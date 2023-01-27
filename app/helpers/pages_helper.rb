376261595a3978ce958b9427ead96a658a2a43fd
Umer Jamil <52040321+UmerJamil5854@users.noreply.github.com>
2023-01-27 14:44:39 -0500

    Revert "- Added the skeleton Home (Default)"

--- app/helpers/pages_helper.rb
+++
@@ -1,8 +0,0 @@
-module PagesHelper
-
-  def app_title
-    title = ENV["APPLICATION_TITLE"] ? ENV["APPLICATION_TITLE"] : 'My Website'
-    return title.html_safe
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get "about", to: "pages#about"
-  get "contact", to: "pages#contact"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#home"
+  # root "articles#index"
 end
